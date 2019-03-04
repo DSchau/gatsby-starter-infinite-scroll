e6dfa64226653c9449642f2c60b7aa80bc525c9c
Atte Juvonen <atte.juvonen@futurice.com>
2019-03-04 20:33:29 +0200

    WIP

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -11,52 +11,48 @@ const Pagination = props => {
     const isLast = currentPage === countPages;
     const prevPage = "/" + (currentPage - 1 > 1 ? (currentPage - 1) : "");
     const nextPage = "/" + (currentPage + 1);
+    const verticalAlignment = { paddingTop: "0.25em" }
 
     return (
         <React.Fragment>
             <div className="pagination">
 
-                {/* "Prev" arrow and text */}
+                {/* "Prev" arrow */}
                 {!isFirst && (
-                    <Link to={prevPage} rel="prev">
+                    <Link to={prevPage} rel="prev" style={verticalAlignment}>
                         <span className="prev-arrow">
                             <FaArrowLeft/>
                         </span>
-                        <h4 className="prev-link-text">
-                            {/*Prev*/}
-                        </h4>
                     </Link>
                 )}
 
-                {/* Numbered page links. TODO: prevent "overflowing" page links when many links and tiny screen. */}
+                {/* Numbered page links. */}
                 {countPages > 1 && (
-                    <ul className="pagination-numbers">
-                        
+                    <React.Fragment>
                         {Array.from({ length: countPages }, (_, i) => (
-                            <li key={`page-${i + 1}`} style={{ margin: 0 }}>
-                                <Link
-                                    to={`/${i === 0 ? "" : i + 1}`}
-                                    style={{
-                                        padding: "3px 8px",
-                                        borderRadius: "5px",
-                                        textDecoration: "none",
-                                        color: i + 1 === currentPage ? "#ffffff" : "",
-                                        background: i + 1 === currentPage ? theme.color.brand.primary : ""
-                                    }}
-                                >
-                                    {i + 1}
-                                </Link>
-                            </li>
+                            <Link
+                                key={`page-${i + 1}`} 
+                                to={`/${i === 0 ? "" : i + 1}`}
+                                style={{
+                                    padding: "3px 8px",
+                                    borderRadius: "5px",
+                                    textDecoration: "none",
+                                    color: i + 1 === currentPage ? "#ffffff" : "#666",
+                                    background: i + 1 === currentPage ? theme.color.brand.primary : "",
+                                    lineHeight: "30px",
+                                    verticalAlign: "middle"
+                                }}
+                                className="pagination-numbers"
+                            >
+                                {i + 1}
+                            </Link>
                         ))}
-                    </ul>
+                    </React.Fragment>
                 )}
 
-                {/* "Next" arrow and text */}
+                {/* "Next" arrow */}
                 {!isLast && (
-                    <Link to={nextPage} rel="next">
-                        <h4 className="next-link-text">
-                            {/*Next*/}
-                        </h4> 
+                    <Link to={nextPage} rel="next" style={verticalAlignment}>
                         <span className="next-arrow">
                             <FaArrowRight/>
                         </span>
@@ -68,77 +64,56 @@ const Pagination = props => {
             <style jsx>{`
                 .next-arrow {
                     :global(svg) {
-                    margin-left: 10px !important;
+                        margin-left: 10px !important;
                     }
+                }
+
+                .next-arrow .prev-arrow {
+                    padding-top: 100px;
+                }
+
+                a.pagination-numbers:hover {
+                    background: ${theme.color.brand.primaryLight};
                 }
             
                 .pagination {
                     display: flex;
+                    maxWidth: 700px;
+                    flex-wrap: wrap;
                     flex-direction: row;
                     justify-content: center;
                     padding: ${theme.space.l} ${theme.space.l} ${theme.space.l};
                     margin: ${theme.space.stack.l};
 
-                    .pagination-numbers {
-                    display: flex;
-                    flexWrap: wrap;
-                    maxWidth: 700px;
-                    margin: 0 0 0 0;
-                    alignItems: center;
-                    list-style-type: none;
-                    padding: 0;
-                    lineHeight: 30px;
+                    .flexItem {
+
+                    }
+
                     
-                    :global(a):hover {
-                        background: ${theme.color.brand.primaryLight};
+
+                    :global(a:nth-child(2)) {
+                        margin: 0;
+                    }
+
+                    :global(svg) {
+                        fill: ${theme.color.special.attention};
+                        width: ${theme.space.m};
+                        height: ${theme.space.m};
+                        flex-shrink: 0;
+                        flex-grow: 0;
+                        transition: all 0.5s;
+                        margin: ${theme.space.inline.s};
                     }
                 }
+            
 
-
-                .next-link-text {
-                    color: &color-brand-primary;
-                    width: 100%;
-                    text-align: right;
-                    margin-left: 20px;
-                }
-                .prev-link-text {
-                    color: &color-brand-primary;
-                    padding-right: 20px;
-                }
-
-                :global(a) {
-                    display: flex;
-                }
-
-                :global(a:nth-child(2)) {
-                    margin: 0;
-                }
-
-
-
-                h4 {
-                    font-weight: 600;
-                    margin: 0;
-                    font-size: 1.1em;
-                }
-                :global(svg) {
-                    fill: ${theme.color.special.attention};
-                    width: ${theme.space.m};
-                    height: ${theme.space.m};
-                    flex-shrink: 0;
-                    flex-grow: 0;
-                    transition: all 0.5s;
-                    margin: ${theme.space.inline.s};
-                }
-            }
-
-            @from-width desktop {
-                @media (hover: hover) {
-                    .pagination :global(a:hover svg) {
-                        transform: scale(1.5);
+                @from-width desktop {
+                    @media (hover: hover) {
+                        .pagination :global(a:hover svg) {
+                            transform: scale(1.5);
+                        }
                     }
                 }
-            }
             `}
             </style>
         </React.Fragment>
